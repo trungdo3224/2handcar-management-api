89bed6fdacb3940cb02f5a477ff58965209f8a37
trungdo3224 <trungdo3224@gmail.com>
2023-07-14 09:16:59 +0700

    feat: change reports id data type to number

--- src/reports/report.entity.ts
+++ src/reports/report.entity.ts
@@ -3,7 +3,7 @@ import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
 @Entity()
 export class Report {
   @PrimaryGeneratedColumn()
-  id: string;
+  id: number;
 
   @Column()
   car_name: string;
