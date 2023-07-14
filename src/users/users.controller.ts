f48bbb93b72868f1508a125b24df66d16b749b5d
trungdo3224 <trungdo3224@gmail.com>
2023-07-14 15:25:08 +0700

    feat: add create user dto and signup route

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
+import { CreateUserDto } from './dtos/create-user.dto';
 
-@Controller('users')
-export class UsersController {}
+@Controller('auth')
+export class UsersController {
+  @Post('/signup')
+  createUser(@Body() body: CreateUserDto) {
+    return body;
+  }
+
+  @Get('/:id')
+  findOne() {
+
+  }
+}
