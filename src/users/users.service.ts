4dea0a0906f54fa8e315bc16b774506cbcb2d737
trungdo3224 <trungdo3224@gmail.com>
2023-07-14 17:43:33 +0700

    feat: add create, findOne, find method for user service

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -7,9 +7,33 @@ import { User } from './user.entity';
 export class UsersService {
   constructor(@InjectRepository(User) private repo: Repository<User>) {}
 
-  async create(email: string, password: string) {
-    const user = await this.repo.create({ email, password });
-
+  create(email: string, password: string) {
+    const user = this.repo.create({ email, password });
+ 
     return this.repo.save(user);
   }
+
+  async findOne(id: number) {
+    const findOptions = {
+      where: {
+        id,
+      }
+    }
+    const user = await this.repo.findOne(findOptions);
+    return user;
+  }
+  
+  find(email: string) {
+    const findOptions = {
+      where: {
+        email
+      }
+    }
+    return this.repo.find(findOptions)
+  }
+
+  update(id: number) {}
+
+  remove(id: number) {}
+
 }
